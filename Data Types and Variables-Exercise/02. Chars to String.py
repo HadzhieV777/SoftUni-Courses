d772a0939b215e54bba58670f288cc21a528eb87
Hadzhiev <bdwhadjiev42@gmail.com>
2021-08-14 15:21:31 +0300

    Uploaded problem #02. Chars to String from the Data Types and Variables-Exercise

---
+++ Data Types and Variables-Exercise/02. Chars to String.py
@@ -0,0 +1,12 @@
+# 02. Chars to String
+char_one = input()
+char_two = input()
+char_three = input()
+
+
+def char_to_string():
+    string_all = char_one + char_two + char_three
+    print(string_all)
+
+
+char_to_string()
