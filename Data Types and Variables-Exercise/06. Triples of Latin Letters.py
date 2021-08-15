25ee57b7ae6f4998520484b13b0719b24a562abe
Hadzhiev <bdwhadjiev42@gmail.com>
2021-08-15 11:07:32 +0300

    Uploaded problem #06. Triples of Latin Letters from the Data Types and Variables-Exercise

---
+++ Data Types and Variables-Exercise/06. Triples of Latin Letters.py
@@ -0,0 +1,7 @@
+# 06. Triples of Latin Letters
+n = int(input())
+
+for i in range(0, n):
+    for j in range(0, n):
+        for k in range(0, n):
+            print(f'{chr(i + 97)}{chr(j + 97)}{chr(k + 97)}')
