7fcc6f82d38465144ad19feb0358c5d65f9dbe86
Hadzhiev <bdwhadjiev42@gmail.com>
2021-08-18 09:31:51 +0300

    Uploaded problem #06. Survival of the Biggest from the Lists-Basics-Exercise

---
+++ Lists_Basics-Excercise/06-Survival_of_the_biggest.py
@@ -0,0 +1,16 @@
+# 06. Survival of the Biggest
+numbers_list = input().split()
+n = int(input())
+
+for i in range (len(numbers_list)):
+    numbers_list[i] = int(numbers_list[i])
+
+for i in range(n):
+    min_element = min(numbers_list)
+
+    numbers_list.remove(min_element)
+
+for i in range (len(numbers_list)):
+    numbers_list[i] = str(numbers_list[i])
+
+print(', '.join((numbers_list)))
