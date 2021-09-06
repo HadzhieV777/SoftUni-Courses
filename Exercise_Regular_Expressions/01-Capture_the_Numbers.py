3128eb0dbbc967c48c218ea5a119161f6c9dc61f
Hadzhiev <bdwhadjiev42@gmail.com>
2021-09-06 09:55:32 +0300

    Uploaded Problem #01. Capture the Numbers from Exercise_Regular_Expressions

---
+++ Exercise_Regular_Expressions/01-Capture_the_Numbers.py
@@ -0,0 +1,16 @@
+# 01. Capture the Numbers
+import re
+
+line = input()
+
+pattern = r"\d+"
+numbers = []
+
+while line:
+    match = re.findall(pattern, line)
+    if match:
+        numbers.extend(match)
+
+    line = input()
+
+print(*numbers, sep=" ")
