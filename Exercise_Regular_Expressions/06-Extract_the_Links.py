1a0e659b3d59c9891c8a1564c528f9469af71d91
Hadzhiev <bdwhadjiev42@gmail.com>
2021-09-08 09:13:44 +0300

    Uploaded Problem #06. Extract the Links from Exercise Regular Expressions

---
+++ Exercise_Regular_Expressions/06-Extract_the_Links.py
@@ -0,0 +1,10 @@
+06. Extract the Links
+import re
+
+data = input()
+pattern = r'(^|(?<=\s))w{3}\.[A-Za-z0-9-]+(\.[a-z]+)+($|(?=\s))'
+
+while data:
+    for el in re.finditer(pattern, data):
+        print(el.group())
+    data = input()
